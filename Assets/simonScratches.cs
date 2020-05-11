0201bfc4bb0af347a3112b994f56f3db7fab25e7
TasThing <lufarionite@gmail.com>
2020-05-11 12:09:02 -0400

    Add speed up effect

--- Assets/simonScratches.cs
+++ Assets/simonScratches.cs
@@ -14,13 +14,18 @@ public class simonScratches : MonoBehaviour
 
     public KMSelectable startButton;
     public KMSelectable armButton;
+    public KMSelectable armDial;
     public Color[] recordColors;
 
     public Renderer record;
     public Transform arm;
 
+    private bool isCCW;
+
+    private Coroutine recordMovement;
+    private bool spinning;
+    private bool fullSpeed;
     private bool armOnRecord;
-    private float savedElapsed;
 
     private static int moduleIdCounter = 1;
     private int moduleId;
@@ -30,26 +35,79 @@ public class simonScratches : MonoBehaviour
     {
         moduleId = moduleIdCounter++;
         armButton.OnInteract += delegate () { PressArm(); return false; };
+        startButton.OnInteract += delegate () { PressStart(); return false; };
+        armDial.OnInteract += delegate () { PressArmDial(); return false; };
     }
 
     void Start()
     {
+        isCCW = rnd.Range(0, 2) == 0;
         record.materials[1].color = recordColors.PickRandom();
         record.materials[0].color = recordColors.Where(x => record.materials[1].color != x).PickRandom();
     }
 
     void PressArm()
     {
-        if (armOnRecord)
+        if (armOnRecord || !spinning || moduleSolved)
             return;
-        StartCoroutine(MoveArm());
+        StartCoroutine(MoveArm(arm.localEulerAngles.y, 30f));
     }
 
-    IEnumerator MoveArm()
+    void PressStart()
+    {
+        audio.PlaySoundAtTransform("click", startButton.transform);
+        startButton.AddInteractionPunch(.1f);
+        if (spinning || moduleSolved)
+            return;
+        StartCoroutine(StartRecordSpin());
+    }
+
+    void PressArmDial()
+    {
+
+    }
+
+    IEnumerator StartRecordSpin()
+    {
+        spinning = true;
+        var elapsed = 0f;
+        var duration = 4f;
+        while (elapsed < duration)
+        {
+            var accel = Mathf.Lerp(0f, 100f, elapsed / duration);
+			var framerate = 1f / Time.deltaTime;
+			var rotation = accel / framerate;
+			if (isCCW)
+				rotation *= -1;
+			var y = record.transform.localEulerAngles.y;
+			y += rotation;
+			record.transform.localEulerAngles = new Vector3(0f, y, 90f);
+			yield return null;
+			elapsed += Time.deltaTime;
+        }
+        recordMovement = StartCoroutine(SpinRecord());
+        yield break;
+    }
+
+    IEnumerator SpinRecord()
+    {
+        fullSpeed = true;
+        while (true)
+		{
+			var framerate = 1f / Time.deltaTime;
+			var rotation = 100f / framerate;
+			if (isCCW)
+				rotation *= -1;
+			var y = record.transform.localEulerAngles.y;
+			y += rotation;
+			record.transform.localEulerAngles = new Vector3(0f, y, 90f);
+			yield return null;
+		}
+    }
+
+    IEnumerator MoveArm(float startRotation, float endRotation)
     {
         armOnRecord = true;
-        var startRotation = arm.localEulerAngles.y;
-        var endRotation = 35f;
         var elapsed = 0f;
         var duration = 1f;
         while (elapsed < duration)
@@ -58,7 +116,7 @@ public class simonScratches : MonoBehaviour
             yield return null;
             elapsed += Time.deltaTime;
         }
-        arm.localEulerAngles = new Vector3(0f, 35f, 0f);
+        arm.localEulerAngles = new Vector3(0f, endRotation, 0f);
         module.GetComponent<KMSelectable>().Children[0] = null;
         module.GetComponent<KMSelectable>().UpdateChildren();
     }
